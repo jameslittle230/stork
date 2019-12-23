ca8465e3c90ff5762ad0437a5fe149cd8f1f2f38
James Little <littleguy23@gmail.com>
2019-12-23 16:09:13 -0500

    Improve results output to DOM, add styling

--- src/index.js
+++ src/index.js
@@ -2,18 +2,30 @@ import("../pkg/index.js")
   .catch(console.error)
   .then(module => {
     search = module.search;
+    for (name in entities) {
+      if (entities[name]["inputElement"].value) {
+        performSearch(name, entities[name]["inputElement"].value);
+      }
+    }
   });
 
 var entities = {};
 export var search;
 
+const messageTemplate = `<div class="stork-message">{{message}}</div>`;
+
+const htmlResultTemplate = `<li class="stork-result">
+    <a href="{{link}}">
+        <p class="stork-title">{{title}}</p>
+        <p class="stork-excerpt">{{excerpt}}</p>
+    </a>
+</div>`;
+
 export function register(name, url) {
   entities[name] = {};
   var r = new XMLHttpRequest();
-  r.addEventListener("load", function(event) {
-    let response = event.target.response;
-    console.log(`${name}: ${response.byteLength} bytes loaded`);
-    entities[name]["index"] = new Uint8Array(response);
+  r.addEventListener("load", e => {
+    handleLoadedIndex(name, e);
   });
   r.responseType = "arraybuffer";
   r.open("GET", "http://localhost:8000/out.stork");
@@ -24,44 +36,83 @@ export function register(name, url) {
   entities[name]["inputElement"] = document.querySelector(
     `input[data-stork=${name}]`
   );
-  entities[name]["inputElement"].addEventListener("input", performSearch);
+  entities[name]["inputElement"].addEventListener("input", handleInputEvent);
+}
+
+function handleLoadedIndex(name, event) {
+  let response = event.target.response;
+  console.log(`${name}: ${response.byteLength} bytes loaded`);
+  entities[name]["index"] = new Uint8Array(response);
+  if (entities[name]["inputElement"].value) {
+    performSearch(name, entities[name]["inputElement"].value);
+  }
 }
 
 async function resolveSearch(index, query) {
-  // return new Promise(resolve => {
+  if (!search) {
+    return null;
+  }
   return Array.from(JSON.parse(search(index, query)));
-  // });
 }
 
-function performSearch(event) {
-  let name = "federalist";
+function handleInputEvent(event) {
+  let name = event.target.getAttribute("data-stork");
   let query = event.target.value;
+
   if (!entities[name]["index"]) {
     return;
   }
+
+  performSearch(name, query);
+}
+
+function performSearch(name, query) {
+  var message = "...";
+  var resultString = "";
+
+  if (query && query.length >= 3) {
+    let results = resolveSearch(entities[name]["index"], query).then(
+      results => {
+        if (results && results.length === 0) {
+          message = "No results found.";
+        } else if (results && results.length > 0) {
+          message = `${results.length} results found.`;
+          for (let i = 0; i < results.length; i++) {
+            let link = results[i]["url"];
+            let title = results[i]["title"];
+            let excerpt = results[i]["excerpt"];
+            let listItem = htmlResultTemplate
+              .replace("{{link}}", link)
+              .replace("{{title}}", title)
+              .replace("{{excerpt}}", excerpt)
+              .replace(query, `<span>${query}</span>`);
+            resultString += listItem;
+          }
+        }
+        updateDom(name, message, resultString);
+      }
+    );
+  } else {
+    updateDom(name, message, "");
+  }
+}
+
+function updateDom(name, message, resultString) {
   while (entities[name]["listElement"].firstChild) {
     entities[name]["listElement"].removeChild(
       entities[name]["listElement"].firstChild
     );
   }
-  if (query && query.length >= 3) {
-    let results = resolveSearch(entities[name]["index"], query).then(
-      results => {
-        if (results.length === 0) {
-          let text = document.createTextNode("No results found.");
-          entities[name]["listElement"].appendChild(text);
-          return;
-        }
 
-        for (let i = 0; i < results.length; i++) {
-          let li = document.createElement("li");
-          li.appendChild(document.createTextNode(JSON.stringify(results[i])));
-          entities[name]["listElement"].appendChild(li);
-        }
-      }
-    );
-  } else {
-    let text = document.createTextNode("...");
-    entities[name]["listElement"].appendChild(text);
+  let messageElements = document.getElementsByClassName("stork-message");
+  for (let elem of messageElements) {
+    elem.remove();
   }
+
+  let messageString = messageTemplate.replace("{{message}}", message);
+  entities[name]["listElement"].insertAdjacentHTML(
+    "beforebegin",
+    messageString
+  );
+  entities[name]["listElement"].innerHTML = resultString;
 }
