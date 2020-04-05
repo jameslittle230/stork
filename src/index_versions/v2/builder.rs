795f8a29a4dfc8cc7b382754a74d145d96dcfb65
James Little <littleguy23@gmail.com>
2020-04-05 16:51:25 -0700

    Add reverse stems as aliases in index

--- src/index_versions/v2/builder.rs
+++ src/index_versions/v2/builder.rs
@@ -5,13 +5,18 @@ use std::fs::File;
 use std::io::{BufReader, Read};
 use std::path::Path;
 
+extern crate rust_stemmers;
+use rust_stemmers::{Algorithm, Stemmer};
+
 pub fn build(config: &Config) -> Index {
+    let en_stemmer = Stemmer::create(Algorithm::English);
     let mut entries: Vec<Entry> = Vec::new();
-    let mut queries: HashMap<String, Container> = HashMap::new();
+    let mut containers: HashMap<String, Container> = HashMap::new();
+
     let config = &config.input;
 
+    // Step 1: Fill entries vector
     let base_directory = Path::new(&config.base_directory);
-
     for stork_file in config.files.as_ref().unwrap_or(&vec![]).iter() {
         let full_pathname = &base_directory.join(&stork_file.path);
         let file = File::open(&full_pathname).unwrap();
@@ -30,6 +35,26 @@ pub fn build(config: &Config) -> Index {
         entries.push(entry);
     }
 
+    let mut stems: HashMap<String, Vec<String>> = HashMap::new();
+
+    for entry in &entries {
+        let words: Vec<String> = entry
+            .contents
+            .split_whitespace()
+            .map(|w| w.to_string())
+            .collect();
+
+        for word in words {
+            let normalized_word = remove_surrounding_punctuation(&word.to_lowercase());
+            let stem = en_stemmer.stem(&normalized_word).to_string();
+            let stem_vector = stems.entry(stem).or_insert(vec![]);
+            if !stem_vector.contains(&normalized_word) {
+                stem_vector.push(normalized_word);
+            }
+        }
+    }
+
+    // Step 2: Fill containers map
     for (entry_index, entry) in entries.iter().enumerate() {
         let words_in_contents: Vec<String> = entry
             .contents
@@ -41,29 +66,55 @@ pub fn build(config: &Config) -> Index {
             let normalized_word = remove_surrounding_punctuation(&word.to_lowercase());
             let normalized_word_len = &normalized_word.len();
 
-            let entry_result: &mut SearchResult = queries
+            // Step 2A: Fill the container's results map
+            let results_map = &mut containers
                 .entry(normalized_word.clone())
                 .or_insert_with(Container::new)
-                .results
+                .results;
+
+            let entry_result: &mut SearchResult = results_map
                 .entry(entry_index)
                 .or_insert_with(SearchResult::new);
 
             entry_result.excerpts.push(Excerpt { word_index });
 
+            // Step 2B: Fill _other containers'_ aliases maps with the
+            // prefixes of this word
             for n in 3..*normalized_word_len {
                 let substring = &normalized_word.as_str()[0..n].to_string();
 
-                let _alias_score = queries
+                let alises_map = &mut containers
                     .entry(substring.clone())
                     .or_insert_with(Container::new)
-                    .aliases
+                    .aliases;
+
+                let _alias_score = alises_map
                     .entry(normalized_word.clone())
                     .or_insert(127 - (*normalized_word_len - n) as u8);
+            }
+
+            // Step 2C: Fill _other containers'_ alias maps with the
+            // reverse-stems of this word
+            let stem = en_stemmer.stem(&normalized_word).to_string();
+            if let Some(reverse_stems_vector) = stems.get(&stem) {
+                for reverse_stem in reverse_stems_vector {
+                    if reverse_stem != &normalized_word {
+                        let _alias_score = containers
+                            .entry(reverse_stem.clone())
+                            .or_insert_with(Container::new)
+                            .aliases
+                            .entry(normalized_word.clone())
+                            .or_insert(50 as u8);
+                    }
+                }
             }
         }
     }
 
-    Index { entries, queries }
+    Index {
+        entries,
+        queries: containers,
+    }
 }
 
 fn remove_surrounding_punctuation(input: &str) -> String {
