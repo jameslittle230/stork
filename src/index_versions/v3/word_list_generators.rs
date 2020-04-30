689059bee5e00dd20f39f5f1e51d7e7eba1ba744
James Little <littleguy23@gmail.com>
2020-04-29 17:44:19 -0700

    Implement SRTWordListParser

--- src/index_versions/v3/word_list_generators.rs
+++ src/index_versions/v3/word_list_generators.rs
@@ -24,8 +24,25 @@ impl WordListGenerator for PlainTextWordListGenerator {
 pub(super) struct SRTWordListGenerator {}
 
 impl WordListGenerator for SRTWordListGenerator {
-    fn create_word_list(&self, _buffer: &str) -> Contents {
-        Contents { word_list: vec![] }
+    fn create_word_list(&self, buffer: &str) -> Contents {
+        let subs = srtparse::from_str(buffer.as_bytes()).expect("Can't parse SRT file.");
+        let mut word_list: Vec<AnnotatedWord> = Vec::new();
+
+        for sub in subs {
+            for word in sub.text.split_whitespace() {
+                word_list.push({
+                    let mut fields = HashMap::new();
+                    fields.insert("time".to_string(), format!("{}", sub.start_time));
+
+                    AnnotatedWord {
+                        word: word.to_string(),
+                        fields,
+                    }
+                })
+            }
+        }
+
+        Contents { word_list }
     }
 }
 
