b45c7791fd22285c96e8ddf59f8ea5d61a9f5514
James Little <littleguy23@gmail.com>
2020-05-25 13:29:07 -0700

    Lint JS and TS independently

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,37 @@
 module.exports = {
-  root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint"
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ["plugin:prettier/recommended"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {},
+      {
+        usePrettierrc: true
+      }
+    ]
+  },
+  overrides: [
+    {
+      files: ["*.ts"],
+      parser: "@typescript-eslint/parser",
+      plugins: ["@typescript-eslint"],
+      extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier/@typescript-eslint"
+      ]
+    }
   ]
 };
