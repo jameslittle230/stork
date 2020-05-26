e4b19d3fcffef503c2ec98b3b0d2558732e6bd44
James Little <littleguy23@gmail.com>
2020-05-25 19:11:32 -0700

    Code coverage?

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  modulePathIgnorePatterns: ["<rootDir>/target/*", "<rootDir>/pkg/*"]
+  modulePathIgnorePatterns: ["<rootDir>/target/*", "<rootDir>/pkg/*"],
+  collectCoverageFrom: ["js/**/*.{js,ts}"]
 };
