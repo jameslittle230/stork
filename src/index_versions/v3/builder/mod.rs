19cbc100ca9c7570914fd49106d2a40115010cfc
James Little <littleguy23@gmail.com>
2020-08-09 11:51:57 -0700

    Refactor steps into functions

--- src/index_versions/v3/builder/mod.rs
+++ src/index_versions/v3/builder/mod.rs
@@ -35,150 +35,167 @@ impl fmt::Display for IndexGenerationError {
 }
 
 pub fn build(config: &Config) -> Result<Index, IndexGenerationError> {
-    let mut intermediate_entries: Vec<IntermediateEntry> = Vec::new();
-    let mut containers: HashMap<String, Container> = HashMap::new();
-
     println!("{}", Nudger::from(config).generate_formatted_output());
 
-    // Step 1: Fill entries vector
-    let base_directory = Path::new(&config.input.base_directory);
-    for stork_file in config.input.files.iter() {
-        let filetype = &stork_file.computed_filetype().unwrap_or_else(|| panic!("Cannot determine a filetype for {}. Please include a filetype field in your config file or use a known file extension.", &stork_file.title));
+    fn fill_intermediate_entries(
+        config: &Config,
+        intermediate_entries: &mut Vec<IntermediateEntry>,
+    ) -> Result<(), IndexGenerationError> {
+        let base_directory = Path::new(&config.input.base_directory);
+        for stork_file in config.input.files.iter() {
+            let filetype = &stork_file.computed_filetype().unwrap_or_else(|| panic!("Cannot determine a filetype for {}. Please include a filetype field in your config file or use a known file extension.", &stork_file.title));
 
-        let buffer: String = match &stork_file.source {
-            DataSource::Contents(contents) => contents.to_string(),
-            DataSource::FilePath(path_string) => {
-                let full_pathname = &base_directory.join(&path_string);
-                let file = File::open(&full_pathname).unwrap();
-                let mut buf_reader = BufReader::new(file);
-                let mut buffer = String::new();
-                let _bytes_read = buf_reader.read_to_string(&mut buffer);
-                buffer
-            }
-            DataSource::URL(_url) => panic!("URL not available yet"),
-        };
+            let buffer: String = match &stork_file.source {
+                DataSource::Contents(contents) => contents.to_string(),
+                DataSource::FilePath(path_string) => {
+                    let full_pathname = &base_directory.join(&path_string);
+                    let file = File::open(&full_pathname).unwrap();
+                    let mut buf_reader = BufReader::new(file);
+                    let mut buffer = String::new();
+                    let _bytes_read = buf_reader.read_to_string(&mut buffer);
+                    buffer
+                }
+                DataSource::URL(_url) => panic!("URL not available yet"),
+            };
 
-        let current_stem_config = stork_file
-            .stemming_override
-            .clone()
-            .unwrap_or_else(|| config.input.stemming.clone());
+            let current_stem_config = stork_file
+                .stemming_override
+                .clone()
+                .unwrap_or_else(|| config.input.stemming.clone());
 
-        let stem_algorithm: Option<Algorithm> = match current_stem_config {
-            StemmingConfig::Language(alg) => Some(alg.to_owned()),
-            StemmingConfig::None => None,
-        };
+            let stem_algorithm: Option<Algorithm> = match current_stem_config {
+                StemmingConfig::Language(alg) => Some(alg.to_owned()),
+                StemmingConfig::None => None,
+            };
 
-        let mut per_file_input_config = config.input.clone();
-        per_file_input_config.html_selector = stork_file.html_selector_override.clone();
-        let contents: Contents = returns_word_list_generator(filetype)
-            .create_word_list(&per_file_input_config, buffer.as_str())
-            .map_err(IndexGenerationError::WordListGenerationError)?;
+            let mut per_file_input_config = config.input.clone();
+            per_file_input_config.html_selector = stork_file.html_selector_override.clone();
+            let contents: Contents = returns_word_list_generator(filetype)
+                .create_word_list(&per_file_input_config, buffer.as_str())
+                .map_err(IndexGenerationError::WordListGenerationError)?;
 
-        let entry = IntermediateEntry {
-            contents,
-            stem_algorithm,
-            title: stork_file.title.clone(),
-            url: stork_file.url.clone(),
-            fields: stork_file.fields.clone(),
-        };
+            let entry = IntermediateEntry {
+                contents,
+                stem_algorithm,
+                title: stork_file.title.clone(),
+                url: stork_file.url.clone(),
+                fields: stork_file.fields.clone(),
+            };
 
-        intermediate_entries.push(entry);
+            intermediate_entries.push(entry);
+        }
+
+        Ok(())
     }
 
-    let mut stems: HashMap<String, Vec<String>> = HashMap::new();
+    let mut intermediate_entries: Vec<IntermediateEntry> = Vec::new();
+    fill_intermediate_entries(&config, &mut intermediate_entries)?;
 
-    for entry in &intermediate_entries {
-        let contents = &entry.contents;
+    fn fill_stems(
+        intermediate_entries: &Vec<IntermediateEntry>,
+        stems: &mut HashMap<String, Vec<String>>,
+    ) {
+        for entry in intermediate_entries {
+            let contents = &entry.contents;
 
-        if let Some(stem_algorithm) = entry.stem_algorithm {
-            for annotated_word in contents.word_list.iter() {
-                let normalized_word =
-                    remove_surrounding_punctuation(&annotated_word.word.to_lowercase());
-                let stem = Stemmer::create(stem_algorithm)
-                    .stem(&normalized_word)
-                    .to_string();
-                let stem_vector = stems.entry(stem).or_insert_with(Vec::default);
-                if !stem_vector.contains(&normalized_word) {
-                    stem_vector.push(normalized_word);
+            if let Some(stem_algorithm) = entry.stem_algorithm {
+                for annotated_word in contents.word_list.iter() {
+                    let normalized_word =
+                        remove_surrounding_punctuation(&annotated_word.word.to_lowercase());
+                    let stem = Stemmer::create(stem_algorithm)
+                        .stem(&normalized_word)
+                        .to_string();
+                    let stem_vector = stems.entry(stem).or_insert_with(Vec::default);
+                    if !stem_vector.contains(&normalized_word) {
+                        stem_vector.push(normalized_word);
+                    }
                 }
             }
         }
     }
 
-    // Step 2: Fill containers map
-    for (entry_index, entry) in intermediate_entries.iter().enumerate() {
-        let words_in_title: Vec<AnnotatedWord> = entry
-            .title
-            .split_whitespace()
-            .map(|w| AnnotatedWord {
-                word: w.to_string(),
-                ..Default::default()
-            })
-            .collect();
+    let mut stems: HashMap<String, Vec<String>> = HashMap::new();
+    fill_stems(&intermediate_entries, &mut stems);
 
-        let words_in_contents: Vec<AnnotatedWord> = entry.contents.word_list.to_owned();
+    fn fill_containers(
+        intermediate_entries: &Vec<IntermediateEntry>,
+        stems: &HashMap<String, Vec<String>>,
+        containers: &mut HashMap<String, Container>,
+    ) {
+        for (entry_index, entry) in intermediate_entries.iter().enumerate() {
+            let words_in_title: Vec<AnnotatedWord> = entry
+                .title
+                .split_whitespace()
+                .map(|w| AnnotatedWord {
+                    word: w.to_string(),
+                    ..Default::default()
+                })
+                .collect();
 
-        let word_lists = vec![
-            (WordListSource::Title, words_in_title),
-            (WordListSource::Contents, words_in_contents),
-        ];
+            let words_in_contents: Vec<AnnotatedWord> = entry.contents.word_list.to_owned();
 
-        for (source, word_list) in word_lists {
-            for (word_index, annotated_word) in word_list.iter().enumerate() {
-                let normalized_word =
-                    remove_surrounding_punctuation(&annotated_word.word.to_lowercase());
-                if normalized_word.is_empty() {
-                    break;
-                }
+            let word_lists = vec![
+                (WordListSource::Title, words_in_title),
+                (WordListSource::Contents, words_in_contents),
+            ];
 
-                // Step 2A: Fill the container's results map
-                let results_map = &mut containers
-                    .entry(normalized_word.clone())
-                    .or_insert_with(Container::new)
-                    .results;
+            for (source, word_list) in word_lists {
+                for (word_index, annotated_word) in word_list.iter().enumerate() {
+                    let normalized_word =
+                        remove_surrounding_punctuation(&annotated_word.word.to_lowercase());
+                    if normalized_word.is_empty() {
+                        break;
+                    }
 
-                let entry_result: &mut SearchResult = results_map
-                    .entry(entry_index)
-                    .or_insert_with(SearchResult::new);
-
-                entry_result.excerpts.push(Excerpt {
-                    word_index,
-                    source,
-                    internal_annotations: annotated_word.internal_annotations.clone(),
-                    fields: annotated_word.fields.clone(),
-                });
-
-                // Step 2B: Fill _other containers'_ aliases maps with the
-                // prefixes of this word
-                let chars: Vec<char> = normalized_word.chars().collect();
-                for n in 3..chars.len() {
-                    let substring: String = chars[0..n].iter().collect();
-
-                    let alises_map = &mut containers
-                        .entry(substring.clone())
-                        .or_insert_with(Container::new)
-                        .aliases;
-
-                    let _alias_score = alises_map
+                    // Step 2A: Fill the container's results map
+                    let results_map = &mut containers
                         .entry(normalized_word.clone())
-                        .or_insert(PREFIX_SCORE - (chars.len() - n) as u8);
-                }
+                        .or_insert_with(Container::new)
+                        .results;
 
-                // Step 2C: Fill _other containers'_ alias maps with the
-                // reverse-stems of this word
-                if let Some(stem_algorithm) = entry.stem_algorithm {
-                    let stem = Stemmer::create(stem_algorithm)
-                        .stem(&normalized_word)
-                        .to_string();
-                    if let Some(reverse_stems_vector) = stems.get(&stem) {
-                        for reverse_stem in reverse_stems_vector {
-                            if reverse_stem != &normalized_word {
-                                let _alias_score = containers
-                                    .entry(reverse_stem.clone())
-                                    .or_insert_with(Container::new)
-                                    .aliases
-                                    .entry(normalized_word.clone())
-                                    .or_insert(STEM_SCORE as u8);
+                    let entry_result: &mut SearchResult = results_map
+                        .entry(entry_index)
+                        .or_insert_with(SearchResult::new);
+
+                    entry_result.excerpts.push(Excerpt {
+                        word_index,
+                        source,
+                        internal_annotations: annotated_word.internal_annotations.clone(),
+                        fields: annotated_word.fields.clone(),
+                    });
+
+                    // Step 2B: Fill _other containers'_ aliases maps with the
+                    // prefixes of this word
+                    let chars: Vec<char> = normalized_word.chars().collect();
+                    for n in 3..chars.len() {
+                        let substring: String = chars[0..n].iter().collect();
+
+                        let alises_map = &mut containers
+                            .entry(substring.clone())
+                            .or_insert_with(Container::new)
+                            .aliases;
+
+                        let _alias_score = alises_map
+                            .entry(normalized_word.clone())
+                            .or_insert(PREFIX_SCORE - (chars.len() - n) as u8);
+                    }
+
+                    // Step 2C: Fill _other containers'_ alias maps with the
+                    // reverse-stems of this word
+                    if let Some(stem_algorithm) = entry.stem_algorithm {
+                        let stem = Stemmer::create(stem_algorithm)
+                            .stem(&normalized_word)
+                            .to_string();
+                        if let Some(reverse_stems_vector) = stems.get(&stem) {
+                            for reverse_stem in reverse_stems_vector {
+                                if reverse_stem != &normalized_word {
+                                    let _alias_score = containers
+                                        .entry(reverse_stem.clone())
+                                        .or_insert_with(Container::new)
+                                        .aliases
+                                        .entry(normalized_word.clone())
+                                        .or_insert(STEM_SCORE as u8);
+                                }
                             }
                         }
                     }
@@ -186,6 +203,9 @@ pub fn build(config: &Config) -> Result<Index, IndexGenerationError> {
             }
         }
     }
+
+    let mut containers: HashMap<String, Container> = HashMap::new();
+    fill_containers(&intermediate_entries, &stems, &mut containers);
 
     let entries: Vec<Entry> = intermediate_entries.iter().map(Entry::from).collect();
 
