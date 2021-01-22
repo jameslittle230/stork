66cdaab3c4840f21eeb497734de9ba8111884cee
James Little <littleguy23@gmail.com>
2021-01-21 20:20:52 -0500

    Add tests for WasmQueue module

--- js/__mocks__/stork-search.ts
+++ js/__mocks__/stork-search.ts
@@ -1,1 +1,11 @@
 export const wasm_search = jest.fn;
+export default jest.fn().mockImplementation((input: string) => {
+  return new Promise((res, rej) => {
+    if (input.includes("stork-search.net")) {
+      res();
+      return;
+    } else {
+      rej();
+    }
+  });
+});
