ac555b8786b8351f0f27732e3cf127dfae8e7736
James Little <3058200+jameslittle230@users.noreply.github.com>
2021-02-22 16:50:29 -0800

    Fix build script so it works on fresh clone (#152)

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
+# This script builds the entire project, creating build artifacts for both
+# the stork binary and the Javascript library. It'll also build the Federalist
+# Papers index, just for kicks.
 set -eux
 
+# First, build the Rust components of the project...
 cargo build --release
-yarn webpack -v || yarn install
-yarn build:wasm:prod
-yarn build:test-index:federalist
+yarn build:wasm:prod # ...including the WASM library.
+
+# Install JS dependencies, now that the WASM library has been built
+yarn install
+
+## Build the text index and the JS library
 yarn build:js:prod
+
+# Build the Federalist Papers index, downloading the test corpus if needed
+if [ ! -f "./test/federalist/federalist-1.txt" ]; then
+    ./scripts/download_test_corpora.sh
+fi
+
+yarn build:test-index:federalist
