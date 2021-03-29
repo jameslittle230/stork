7caef1c124a3575e1b980e7ee327527175badd1d
James Little <littleguy23@gmail.com>
2021-03-29 08:43:17 -0700

    Add debug method to JS interface

--- js/loaders/wasmLoader.ts
+++ js/loaders/wasmLoader.ts
@@ -5,10 +5,10 @@ const DEFAULT_WASM_URL = version
   ? `https://files.stork-search.net/stork-${version}.wasm`
   : `https://files.stork-search.net/stork.wasm`;
 
-export const loadWasm = (overrideUrl: string | null): Promise<void> => {
+export const loadWasm = (overrideUrl: string | null): Promise<string> => {
   const url = overrideUrl || DEFAULT_WASM_URL;
   return init(url).then(() => {
     // Suppress the value that init succeeds with.
-    return;
+    return url;
   });
 };
