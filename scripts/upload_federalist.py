187b31968827be8a636b9eaa57701731fe2ef8f5
James Little <littleguy23@gmail.com>
2021-05-18 09:03:04 -0700

    Rewrite upload_federalist script to work properly

--- scripts/upload_federalist.py
+++ scripts/upload_federalist.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import boto3
 import time
 
@@ -9,10 +10,11 @@ if "AWS_ACCESS_KEY_ID" not in os.environ or "AWS_SECRET_ACCESS_KEY" not in os.en
 normalized_dir = filedir = os.path.dirname(os.path.realpath(__file__))
 opj = os.path.join
 
+def uploadFile(localPath, remotePath, extraArgs={}):
+    print(f"Called uploadFile")
 
-def uploadFile(localPath, remotePath, bucket, extraArgs={}):
     s3 = boto3.resource('s3')
-    s3.Bucket(bucket).upload_file(localPath, remotePath, ExtraArgs=extraArgs)
+    s3.Bucket("files-qa.stork-search.net").upload_file(localPath, remotePath, ExtraArgs=extraArgs)
 
 
 def invalidate():
@@ -43,49 +45,30 @@ if __name__ == "__main__":
     binaries = [
         "stork-macos-10-15", 
         "stork-ubuntu-20-04",
-        "stork-ubuntu-16-04",
-        "stork-windows-2019"
+        "stork-ubuntu-16-04"
     ]
 
-    ref = argv[2] # We'll upload to /releases/${ref}/*
+    ref = sys.argv[1] # We'll upload to /releases/${ref}/*
 
-    outof = len(dist_files) + len(binaries)
-    idx = 0
-
-    print(f"Uploading {outof} files...")
+    print(f"Uploading files...")
 
     for file in dist_files:
-        idx += 1
+        for destination_path in [
+           opj("releases", ref, file["filename"]),
+           opj("releases", "latest", file["filename"]),
+           file["filename"]
+        ]:
+            source_path = opj(normalized_dir, "..", "web-artifacts", file["filename"])
 
-        uploadFile(opj(normalized_dir, "..", "dist", file["filename"]),
-                   opj("releases", ref, file["filename"]),
-                   "files.stork-search.net",
-                   {'ContentType': file["contentType"]})
-
-        uploadFile(opj(normalized_dir, "..", "dist", file["filename"]),
-                   opj("releases", "latest", file["filename"]),
-                   "files.stork-search.net",
-                   {'ContentType': file["contentType"]})
-        
-        uploadFile(opj(normalized_dir, "..", "dist", file["filename"]),
-                   file["filename"],
-                   "files.stork-search.net",
-                   {'ContentType': file["contentType"]})
-        
-        print(f"{idx} of {outof} files uploaded to S3.")
+            uploadFile(source_path, destination_path, {'ContentType': file["contentType"]})
     
     for binary in binaries:
-        idx += 1
-
-        uploadFile(opj(normalized_dir, "..", binary),
-                   opj("releases", ref, binary),
-                   "files.stork-search.net")
-
-        uploadFile(opj(normalized_dir, "..", binary),
-                   opj("releases", "latest", binary),
-                   "files.stork-search.net")
-        
-        print(f"{idx} of {outof} files uploaded to S3.")
+        for destination_path in [
+            opj("releases", ref, binary),
+            opj("releases", "latest", binary),
+        ]:
+            source_path = opj(normalized_dir, "..", binary)
+            uploadFile(source_path, destination_path)
 
     invalidate()
     print("Cache invalidated.")
