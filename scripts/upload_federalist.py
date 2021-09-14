12e2250ff5648c4cf43a39028d12263f149daa26
James Little <3058200+jameslittle230@users.noreply.github.com>
2021-09-13 21:57:02 -0400

    Continue working on release build action (#182)

--- scripts/upload_federalist.py
+++ scripts/upload_federalist.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import boto3
 import time
 
@@ -9,10 +10,11 @@ if "AWS_ACCESS_KEY_ID" not in os.environ or "AWS_SECRET_ACCESS_KEY" not in os.en
 normalized_dir = filedir = os.path.dirname(os.path.realpath(__file__))
 opj = os.path.join
 
+def uploadFile(localPath, remotePath, extraArgs={}):
+    print(f"Called uploadFile: {localPath} → {remotePath}")
 
-def uploadFile(localPath, remotePath, bucket, extraArgs={}):
     s3 = boto3.resource('s3')
-    s3.Bucket(bucket).upload_file(localPath, remotePath, ExtraArgs=extraArgs)
+    s3.Bucket("files.stork-search.net").upload_file(localPath, remotePath, ExtraArgs=extraArgs)
 
 
 def invalidate():
@@ -32,25 +34,52 @@ def invalidate():
 
 
 if __name__ == "__main__":
-    dist_files = [
+    web_artifacts = [
         {"filename": "stork.js", "contentType": "text/javascript"},
         {"filename": "stork.wasm", "contentType": "application/wasm"},
-        {"filename": "federalist.st", "contentType": "application/octet-stream"},
         {"filename": "basic.css", "contentType": "text/css"},
         {"filename": "dark.css", "contentType": "text/css"},
     ]
 
-    outof = len(dist_files)
-    idx = 0
+    binaries = [
+        "stork-macos-10-15",
+        "stork-ubuntu-20-04",
+    ]
 
-    print(f"Uploading {outof} files...")
-    for file in dist_files:
-        idx += 1
-        uploadFile(opj(normalized_dir, "..", "dist", file["filename"]),
-                   file["filename"],
-                   "files.stork-search.net",
-                   {'ContentType': file["contentType"]})
-        print(f"{idx} of {outof} files uploaded to S3.")
+    other_files = [
+        "federalist.st"
+    ]
+
+    ref = sys.argv[1] # We'll upload to /releases/${ref}/*
+
+    print(f"Uploading files...")
+
+    for file in web_artifacts:
+        for destination_path in [
+           opj("releases", ref, file["filename"]),
+           opj("releases", "latest", file["filename"]),
+           file["filename"]
+        ]:
+            source_path = opj(normalized_dir, "..", "web-artifacts", file["filename"])
+
+            uploadFile(source_path, destination_path, {'ContentType': file["contentType"]})
+
+    for binary in binaries:
+        for destination_path in [
+            opj("releases", ref, binary),
+            opj("releases", "latest", binary),
+        ]:
+            source_path = opj(normalized_dir, "..", binary, "stork")
+            uploadFile(source_path, destination_path)
+
+    for file in other_files:
+        for destination_path in [
+            opj("releases", ref, file),
+            opj("releases", "latest", file),
+            file
+        ]:
+            source_path = opj(normalized_dir, "..", file)
+            uploadFile(source_path, destination_path)
 
     invalidate()
     print("Cache invalidated.")
