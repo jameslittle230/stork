cef8a66a7d0c963ba77a32841682a534e69987eb
James Little <littleguy23@gmail.com>
2021-12-22 17:38:44 -0500

    Fix index v3 writer

--- stork-index-v3/src/write.rs
+++ stork-index-v3/src/write.rs
@@ -1,3 +1,5 @@
+use std::mem;
+
 use bytes::{BufMut, Bytes, BytesMut};
 
 use crate::Index;
@@ -6,11 +8,15 @@ impl From<&Index> for Bytes {
     fn from(value: &Index) -> Self {
         let index_bytes = rmp_serde::to_vec(&value).unwrap();
         let index_bytes = Bytes::from(index_bytes);
+        let version_bytes = Bytes::from("stork-3");
 
-        let version_bytes = Bytes::from("stork-v3");
+        let mut buf = BytesMut::with_capacity(
+            index_bytes.len() + version_bytes.len() + 2 * mem::size_of::<u64>(),
+        );
+        buf.put_u64(version_bytes.len() as u64);
 
-        let mut buf = BytesMut::with_capacity(index_bytes.len() + version_bytes.len());
-        buf.put(Bytes::from("stork-v3".as_bytes()));
+        buf.put(version_bytes);
+        buf.put_u64(index_bytes.len() as u64);
         buf.put(index_bytes);
 
         buf.freeze()
