927e982485e886c407654649c24101c10e562d70
James Little <3058200+jameslittle230@users.noreply.github.com>
2022-01-08 14:13:26 -0500

    Score results based on multiple excerpts' scores (#233)

--- stork-index-v3/src/search/entry_and_intermediate_excerpts.rs
+++ stork-index-v3/src/search/entry_and_intermediate_excerpts.rs
@@ -156,13 +156,31 @@ impl From<EntryAndIntermediateExcerpts> for Result {
                 TitleBoost::Ridiculous => 5000,
             };
 
-        let score = excerpts.first().map_or(0, |first| first.score) + title_boost_modifier;
+        // Sort each result by a sum of an exponental backoff of its excerpts' scores.
+        // This more evenly weights a single high score excerpt vs multiple low score excerpts.
+        let result_score = {
+            let mut sorted_excerpt_scores: Vec<usize> = excerpts.iter().map(|e| e.score).collect();
+            sorted_excerpt_scores.sort();
+
+            let sum: usize = sorted_excerpt_scores
+                .into_iter()
+                .enumerate()
+                .map(|(index, score)| {
+                    let score = score as f32;
+                    let index = index as f32;
+                    let excerpt_score = score * 2f32.powf(1f32 / (index + 1f32)); // index + 1 to avoid ÷0 errors
+                    excerpt_score as usize
+                })
+                .sum();
+
+            sum + title_boost_modifier
+        };
 
         Result {
             entry: BoundaryEntry::from(entry),
             excerpts,
             title_highlight_ranges,
-            score,
+            score: result_score,
         }
     }
 }
@@ -170,6 +188,65 @@ impl From<EntryAndIntermediateExcerpts> for Result {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn result_with_multiple_excerpts_sorts_higher_than_result_with_single() {
+        let multiple_excerpts = EntryAndIntermediateExcerpts {
+            entry: Entry {
+                contents: "".to_string(),
+                title: "The quick brown fox jumps over the lazy dog".to_string(),
+                url: String::default(),
+                fields: HashMap::default(),
+            },
+            config: PassthroughConfig::default(),
+            intermediate_excerpts: vec![
+                IntermediateExcerpt {
+                    query: "over".to_string(),
+                    entry_index: 0,
+                    score: 128,
+                    source: WordListSource::Title,
+                    word_index: 3,
+                    internal_annotations: Vec::default(),
+                    fields: HashMap::default(),
+                },
+                IntermediateExcerpt {
+                    query: "brown".to_string(),
+                    entry_index: 0,
+                    score: 128,
+                    source: WordListSource::Title,
+                    word_index: 2,
+                    internal_annotations: Vec::default(),
+                    fields: HashMap::default(),
+                },
+            ],
+        };
+
+        let single_excerpt = EntryAndIntermediateExcerpts {
+            entry: Entry {
+                contents: "".to_string(),
+                title: "The quick brown fox jumps over the lazy dog".to_string(),
+                url: String::default(),
+                fields: HashMap::default(),
+            },
+            config: PassthroughConfig::default(),
+            intermediate_excerpts: vec![IntermediateExcerpt {
+                query: "over".to_string(),
+                entry_index: 0,
+                score: 128,
+                source: WordListSource::Title,
+                word_index: 3,
+                internal_annotations: Vec::default(),
+                fields: HashMap::default(),
+            }],
+        };
+
+        let result_multiple_excerpts = Result::from(multiple_excerpts);
+        assert_eq!(result_multiple_excerpts.score, 150);
+
+        let result_single_excerpt = Result::from(single_excerpt);
+        assert_eq!(result_single_excerpt.score, 75);
+    }
 
     #[test]
     fn title_highlight_ranges_are_sorted() {
