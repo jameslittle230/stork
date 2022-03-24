4450a4249a2cf6ec8ffb3a74a2669c0e45896f3b
James Little <littleguy23@gmail.com>
2022-03-23 21:09:34 -0400

    Compress serialized index contents on write

--- stork-lib/src/index_v3/read.rs
+++ stork-lib/src/index_v3/read.rs
@@ -1,5 +1,6 @@
 use super::Index;
 use bytes::Bytes;
+use compression::prelude::*;
 use std::convert::{TryFrom, TryInto};
 
 impl TryFrom<&[u8]> for Index {
@@ -8,14 +9,27 @@ impl TryFrom<&[u8]> for Index {
         let (version_size_bytes, rest) = file.split_at(std::mem::size_of::<u64>());
         let version_size = u64::from_be_bytes(version_size_bytes.try_into().unwrap());
         let version_size: usize = version_size.try_into().unwrap();
-        let (_version_bytes, rest) = rest.split_at(version_size);
+        let (version_bytes, rest) = rest.split_at(version_size);
 
         let (index_size_bytes, rest) = rest.split_at(std::mem::size_of::<u64>());
         let index_size = u64::from_be_bytes(index_size_bytes.try_into().unwrap());
         let index_size: usize = index_size.try_into().unwrap();
         let (index_bytes, _rest) = rest.split_at(index_size);
 
-        rmp_serde::from_read_ref(index_bytes)
+        if version_bytes == Bytes::from("stork-4").as_ref() {
+            rmp_serde::from_read_ref(
+                &index_bytes
+                    .iter()
+                    .cloned()
+                    .decode(&mut BZip2Decoder::new())
+                    .collect::<Result<Vec<_>, _>>()
+                    .unwrap(),
+            )
+        } else if version_bytes == Bytes::from("stork-3") {
+            rmp_serde::from_read_ref(index_bytes)
+        } else {
+            unreachable!()
+        }
     }
 }
 
--- stork-lib/src/index_v3/write.rs
+++ stork-lib/src/index_v3/write.rs
@@ -1,14 +1,20 @@
 use std::mem;
 
 use bytes::{BufMut, Bytes, BytesMut};
+use compression::prelude::*;
 
 use super::Index;
 
 impl From<&Index> for Bytes {
     fn from(value: &Index) -> Self {
         let index_bytes = rmp_serde::to_vec(&value).unwrap();
-        let index_bytes = Bytes::from(index_bytes);
-        let version_bytes = Bytes::from("stork-3");
+        let index_bytes = Bytes::from(
+            index_bytes
+                .encode(&mut BZip2Encoder::new(9), Action::Finish)
+                .collect::<Result<Vec<_>, _>>()
+                .unwrap(),
+        );
+        let version_bytes = Bytes::from("stork-4");
 
         let mut buf = BytesMut::with_capacity(
             index_bytes.len() + version_bytes.len() + 2 * mem::size_of::<u64>(),
