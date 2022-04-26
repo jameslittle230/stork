d7d196d174988bf5b3daeae9bebd1d8ab1d14cdf
James Little <littleguy23@gmail.com>
2022-04-25 22:06:33 -0400

    Add failing tests

--- stork-lib/src/index_v3/build/fill_intermediate_entries/word_list_generators/markdown_word_list_generator.rs
+++ stork-lib/src/index_v3/build/fill_intermediate_entries/word_list_generators/markdown_word_list_generator.rs
@@ -22,6 +22,8 @@ pub fn generate(
 #[cfg(test)]
 mod tests {
 
+    use pretty_assertions::assert_eq;
+
     use crate::{
         config::{File, Filetype, InputConfig, OutputConfig},
         index_v3::build::fill_intermediate_entries::{ReadResult, ReaderConfig},
@@ -29,27 +31,18 @@ mod tests {
 
     use super::generate;
 
-    #[test]
-    fn test_markdown() {
-        let expected = "This is a title Stork should recognize this text This content should be indexed. This is another paragraph with inline text formatting . This is a link. Goodbye!";
+    fn assert_markdown_content(word_list: &str, markdown_content: &str) {
         let computed: String = generate(
             &ReaderConfig {
-                global: InputConfig::default(),
+                global: InputConfig {
+                    frontmatter_handling: crate::config::FrontmatterConfig::Omit,
+                    ..Default::default()
+                },
                 file: File::default(),
                 output: OutputConfig::default(),
             },
             &ReadResult {
-                buffer: r#"
-# This is a title
-
-Stork should recognize this text
-
-- This content should be indexed.
-- This is another paragraph with **_inline text_formatting**.
-- [This is a link.](https://example.com)
-
-Goodbye!"#
-                    .to_string(),
+                buffer: markdown_content.to_string(),
                 filetype: Some(Filetype::Markdown),
                 frontmatter_fields: None,
             },
@@ -61,6 +54,58 @@ Goodbye!"#
         .collect::<Vec<String>>()
         .join(" ");
 
-        assert_eq!(expected, computed);
+        assert_eq!(word_list, computed);
+    }
+
+    #[test]
+    fn test_markdown() {
+        assert_markdown_content(
+            "This is a title Stork should recognize this text This content should be indexed. This is another paragraph with inline text formatting . This is a link. Goodbye!", 
+r#"
+# This is a title
+
+Stork should recognize this text
+
+- This content should be indexed.
+- This is another paragraph with **_inline text_formatting**.
+- [This is a link.](https://example.com)
+
+Goodbye!"#
+);
+    }
+
+    mod issue_290 {
+
+        use super::assert_markdown_content;
+
+        #[test]
+        fn space_after_numeric_list() {
+            assert_markdown_content(
+                "1. something below, there is a space immediately after the 1. above",
+                r#"---
+title: Something
+tags: test
+---
+
+1. 
+
+something below, there is a space immediately after the 1. above  "#,
+            );
+        }
+
+        #[test]
+        fn space_after_bullet_list() {
+            assert_markdown_content(
+                "This is a title Stork should recognize this text This content should be indexed. This is another paragraph with inline text formatting . This is a link. Goodbye!", 
+    r#"---
+title: Something
+tags: test
+---
+
+* 
+
+something below, there is a space immediately after the star above  "#
+    );
+        }
     }
 }
